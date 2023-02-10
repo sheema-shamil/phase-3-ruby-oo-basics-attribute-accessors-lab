d2ca38fc0f34bd79603e0c2be1dbd235e34eef79
sheema <sheemashamilfarook@gmail.com>
2023-02-10 09:08:42 +0300

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+require "pry"
+class Cat
+    attr_accessor :name
+    def meow
+        puts "meow!"
+    end
+end
+
+puss = Cat.new
+
+puss.name=("anisa")
+puss.meaw
+
+binding.pry
